89f66dddbccadf3edfa759264dc5f06aa5a692df
aditya <adityaf.putra@gmail.com>
2015-01-09 21:47:38 +0700

    menambahkan pembentukan rules, baru sampai denormalisasi rules, setelah itu tinggal sorting berdasarkan jumlah. menambahkan fungsi untuk filter mata kuliah yang sudah ditempuh saja. next: cek query untuk mkkhs dan melanjutkan pembentukan rules

--- application/controllers/student/recommendation.php
+++ application/controllers/student/recommendation.php
@@ -105,16 +105,26 @@ class Recommendation extends CI_Controller {
 	       	// get mhs info: fakultas-prodi
 	       	$student = $this->getMhsInfo();
         	
-        	// get MK yang bisa ditempuh mhs join khs, menampilkan semua mata kuliah yang bisa diambil beserta data apakah mk tersebut ditempuh
-        	$mkKhs = $this->getCbMkKhs($student);//print_r($mkKhs);exit;
+        	// get MK yang bisa ditempuh, baik yang pernah diambil maupun belum.
+        	// mhs join khs, menampilkan semua mata kuliah yang bisa diambil beserta data apakah mk tersebut ditempuh
+        	$mkKhs = $this->getCbMkKhs($student);
+//         	print_r($mkKhs);exit;
 //         	echo ($this->getGenericTable($mkKhs, 'table-mk-khs', 'Tabel Matriks MK - KHS'));
+
+        	// filter untuk hanya mengambil MK yang sudah ditempuh
+        	$mkKhs = $this->getCbFilterMkTempuh($mkKhs, 1);
+        	
         	
 	       	// pembentukan matriks
 	       	$mkKhsKomp = $this->getCbMkKhsKomp($student);
+// 	       	print_r($mkKhsKomp);exit;
+	       	
+	       	//membentuk decision tree
 	       	$decisionTree = $this->getCbDecisionTree($mkKhsKomp);
 // 	       	echo ($this->getGenericTable($mkKhsKomp, 'table-mk-khs-komp', 'Tabel Matriks MK - KHS - KOMPETENSI'));
 	       	
 	       	// pembentukan rules
+	       	$rules = $this->getCbRules($decisionTree);
         	
         	
         }
@@ -127,11 +137,36 @@ class Recommendation extends CI_Controller {
         	return $this->m_rekomendasi->getCbMkKhs($student);
         }
         
+        function getCbFilterMkTempuh($mkKhs, $tempuh){
+        	$data = array();
+        	if($tempuh == 1){ // yang sudah ditempuh
+	        	foreach ($mkKhs as $key => $val){ 
+	        		if($val['K_NILAI'] != '0'){
+	        			array_push($data, $val);
+	        		}
+	        	}
+        	}
+        	else if($tempuh == 0){ // belum ditempuh
+        		foreach ($mkKhs as $key => $val){
+        			if($val['K_NILAI'] == '0'){
+        				array_push($data, $val);
+        			}
+        		}  
+        	}
+        	print_r($data);exit;
+        	return $data;
+        }
+        
         function getCbMkKhsKomp($student){
         	return $this->m_rekomendasi->getCbMkKhsKomp($student);
         }
         
         function getCbDecisionTree($data){
+        	/*
+        	 * function get decision tre content based
+        	 * merubah struktur data menjadi x-dimensional
+        	 * nilai x bergantung pada jumlah kompetensi lulusan
+        	 */
         	$tree1 = array();
         	$tree2 = array();
         	foreach ($data as $key => $val){
@@ -156,8 +191,50 @@ class Recommendation extends CI_Controller {
         		$tree2[$val['U1']][$val['U2']][$val['U3']][$val['U4']][$val['U5']][$val['U6']][$val['U7']] = $tree2[$val['U1']][$val['U2']][$val['U3']][$val['U4']][$val['U5']][$val['U6']][$val['U7']] + 1;
         	}
         	
-        	print_r($tree2);exit;
-        	return $tree;
+        	return $tree2;
+        }
+        
+        function getCbRules($rules){
+        	/*
+        	 * function get rules content-based
+        	 * melakukan sorting jumlah leaf, untuk menentukan rules yang paling dominan
+        	 * melakukan merge kombinasi node dari tree dan 
+        	 * menghitung jumlah leaf dari masing-masing kombinasi node yang sama
+        	 */
+        	
+//			sorting, in fact it is no use, karena sudah ter sort otomatis         	
+			ksort($rules);
+// 			print_r($rules); exit;
+			
+
+			$denormalizedRules = array();
+			
+			foreach ($rules as $key => $val){
+				foreach ($val as $key2 => $val2){
+					foreach ($val2 as $key3 => $val3){
+						foreach ($val3 as $key4 => $val4){
+							foreach ($val4 as $key5 => $val5){
+								foreach ($val5 as $key6 => $val6){
+									foreach ($val6 as $key7 => $val7){
+										$tempArr["U1"] = $key;
+										$tempArr["U2"] = $key2;
+										$tempArr["U3"] = $key3;
+										$tempArr["U4"] = $key4;
+										$tempArr["U5"] = $key5;
+										$tempArr["U6"] = $key6;
+										$tempArr["U7"] = $key7;
+										$tempArr["COUNT"] = $val7;
+										
+										array_push($denormalizedRules, $tempArr);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			print_r($denormalizedRules); exit;
         }
         
         function getGenericTable($data, $id, $title){
